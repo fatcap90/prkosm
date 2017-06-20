e49f24aa0e1f83081ff226252b5ad2605619c1c0
Denis <prosvirnin26@gmail.com>
2017-06-20 14:44:14 +0300

    2

--- app/src/main/java/com/example/bronze56k/prkosm/MainActivity.java
+++ app/src/main/java/com/example/bronze56k/prkosm/MainActivity.java
@@ -1,42 +1,79 @@
 package com.example.bronze56k.prkosm;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
-
-import org.osmdroid.api.IMapController;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.MapView;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
 import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.tileprovider.tilesource.XYTileSource;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.MinimapOverlay;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.gestures.RotationGestureOverlay;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
-public class MainActivity extends AppCompatActivity {
+
+
+public class MainActivity extends AppCompatActivity
+{
+    private MyLocationNewOverlay  myLocationoverlay;
+    private MapView  mMapView;
+    private MapController mapController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Context ctx = getApplicationContext();
-        //important! set your user agent to prevent getting banned from the osm servers
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
         setContentView(R.layout.activity_main);
         MapView map = (MapView) findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
-        map.setBuiltInZoomControls(true);
+        map.setClickable(true);
         map.setMultiTouchControls(true);
-        IMapController mapController = map.getController();
+        //final float scale = getResources().getDisplayMetrics().density;
+       // final int newScale = (int) (256 * scale);
+        ///String[] OSMSource = new String[1];
+        //OSMSource[0] = "http://a.tile.openstreetmap.org/";
+       // XYTileSource MapSource = new XYTileSource("OSM", null, 1, 18, newScale, ".png", OSMSource);
+       // map.setTileSource(MapSource);
+        mapController = (MapController) map.getController();
         mapController.setZoom(14);
-        GeoPoint startPoint = new GeoPoint(53.1922, 45.0323);
-        mapController.setCenter(startPoint);
+// My Location Overlay
+        myLocationoverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(this), map);
+        myLocationoverlay.enableMyLocation();
+        myLocationoverlay.setDrawAccuracyEnabled(true);
+        map.getOverlays().add(myLocationoverlay);
+        myLocationoverlay.runOnFirstFix(new Runnable() {
+            public void run() {
+                mapController.animateTo(myLocationoverlay.getMyLocation());
+           }
+      });
     }
 
-    public void onResume(){
+    @Override
+    protected void onResume() {
         super.onResume();
-        //this will refresh the osmdroid configuration on resuming.
-        //if you make changes to the configuration, use
-        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        //Configuration.getInstance().save(this, prefs);
-        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        myLocationoverlay.enableMyLocation();
+        myLocationoverlay.enableFollowLocation();
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 }
